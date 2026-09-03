900206ca85d977a939868d9e9752aeb90f8e4178
typhrenn <julian.galicki@gmail.com>
2026-09-03 13:23:08 +0200

    prepare file function

--- src/preprocessor/preprocessor.c
+++ src/preprocessor/preprocessor.c
@@ -3,12 +3,20 @@
 
 #include "preprocessor/preprocessor.h"
 
-void new_file(Preprocessor *pp, Error ferr, const char *filename, FileData *data) {
+void setup_preprocessor(Preprocessor *pp, ErrorHandler handler, TokenStream *stream) {
+    pp->handler = handler;
+    pp->out = stream;
+    pp->fatal_err = false;
+}
+
+void prepare_file(Preprocessor *pp, Error ferr, const char *filename, FileData *data) {
+
     // later a proper way to check for include loop should be implemented
     if (pp->depth == MAX_INCLUDE_DEPTH) ferr("compiler has reached max include depth, possible include loop encountered");
 
+    // setup
+    sl_push(&pp->included, filename, ferr);
     bl_push(&pp->buffers, data, filename, ferr);
-
     pp->stack[pp->depth] = (IncludeFrame){
         .filename = filename,
         .line = 0,
@@ -18,8 +26,9 @@ void new_file(Preprocessor *pp, Error ferr, const char *filename, FileData *data
     };
 }
 
-void preprocess(FileData data, TokenStream *stream) {
+void preprocess(FileData data, TokenStream *stream, ErrorHandler handler) {
     Preprocessor pp;
-    
-    
+    setup_preprocessor(&pp, handler, stream);
+
+    Token buffer;
 }
